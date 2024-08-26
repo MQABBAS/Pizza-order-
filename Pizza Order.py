0cc465a46c212ec7693d8a56a0d3d74dc9277320
Mohammed Abbas <101219974+MQABBAS@users.noreply.github.com>
2024-08-26 16:17:05 +0100

    Update Pizza Order.py

--- Pizza Order.py
+++ Pizza Order.py
@@ -1,4 +1,4 @@
-print('Wellcome to Python Pizza Deliveries!')
+print('Welcome to Python Pizza Deliveries!')
 size = input('What side pizza do you want? S, M or L')
 add_pepperoni = input('Do you want pepperoni? Y or N')
 extra_cheese = input('Do you want extra cheese? Y or N')
